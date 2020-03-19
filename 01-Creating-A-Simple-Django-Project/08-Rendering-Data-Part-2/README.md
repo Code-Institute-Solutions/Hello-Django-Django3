320a3950c5e15c509c09278c833f96019e2cf397
Tim Nelson <traveltimn@gmail.com>
2020-03-19 15:25:42 +0000

    Add section: Rendering Data Part 2 - Django3

---
+++ 01-Creating-A-Simple-Django-Project/08-Rendering-Data-Part-2/README.md
@@ -0,0 +1,3 @@
+# Hello-Django-Django3
+
+## 08 - Rendering Data Part 2
---
+++ 01-Creating-A-Simple-Django-Project/08-Rendering-Data-Part-2/todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Item
+
+# Create your views here.
+
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)

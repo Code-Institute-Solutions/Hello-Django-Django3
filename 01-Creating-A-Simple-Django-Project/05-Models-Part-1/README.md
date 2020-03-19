2d1a416c29752657593d8b56032d0fad314839cb
Tim Nelson <traveltimn@gmail.com>
2020-03-19 14:49:50 +0000

    Add section: Models Part 1 - Django3

---
+++ 01-Creating-A-Simple-Django-Project/05-Models-Part-1/README.md
@@ -0,0 +1,3 @@
+# Hello-Django-Django3
+
+## 05 - Models Part 1
---
+++ 01-Creating-A-Simple-Django-Project/05-Models-Part-1/todo/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')

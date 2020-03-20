6e4b6bfbd3de0a16fd42f7a123a4429240befddc
Tim Nelson <traveltimn@gmail.com>
2020-03-20 15:19:20 +0000

    Add section: Editing An Item - Django3

---
+++ 01-Creating-A-Simple-Django-Project/12-Editing-An-Item/README.md
@@ -0,0 +1,3 @@
+# Hello-Django-Django3
+
+## 12 - Editing An Item
---
+++ 01-Creating-A-Simple-Django-Project/12-Editing-An-Item/todo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
+
+# Create your views here.
+
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
